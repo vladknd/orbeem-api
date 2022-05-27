9aa312783e8f2814bad44fa5d3664be6154f6b23
Vlad Voznitsa <voznitsa98@gmail.com>
2022-05-27 18:58:34 +0600

    hihihihihiih

--- src/passport.ts
+++ src/passport.ts
@@ -4,7 +4,7 @@ export const steamStrategy = new SteamStrategy(
     {
         returnURL: 'https://orbeem-api.herokuapp.com/api/auth/steam/return',
         realm: 'https://https://orbeem-api.herokuapp.com/',
-        apiKey: 'A8066F8A67DC0B85AD609380454B8065'
+        // apiKey: 'A8066F8A67DC0B85AD609380454B8065'
     },
     function(identifier: any, profile:any, done: any) {
         process.nextTick(function () {
